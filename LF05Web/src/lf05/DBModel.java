9af916d4367d958534d9ba64662335d5ad343569
Vivien.Laasch <Vivien.Laasch@NB045.tqg-hh.local>
2021-10-24 15:24:39 +0200

    more web changes, base for functional servlet, servlet respose tests

--- LF05Web/src/lf05/DBModel.java
+++ LF05Web/src/lf05/DBModel.java
@@ -1,33 +1,77 @@
 package lf05;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.HashMap;
 
 public class DBModel {
-	private Connection db = null;
+	private static Properties props = new Properties();
 
-	public DBModel(String url, String user, String passwd) throws SQLException {
-		db = DriverManager.getConnection(url, user, passwd);
+	private DBModel() {
+
 	}
 
-	public List<DBElement> getTable(String table) throws SQLException{
-		String[] cond = null;
-		return this.getTable(table, "", cond);
+	protected static Connection initDB() throws SQLException, ClassNotFoundException {
+		String file = "lib/db.properties";
+		try (FileInputStream fis = new FileInputStream(file)) {
+			props.load(fis);
+		} catch (IOException e) {
+			// lgr.log(Level.SEVERE, e.getMessage(), e);
+		}
+
+		String url = props.getProperty("db.url");
+		String user = props.getProperty("db.user");
+		String passwd = props.getProperty("db.password");
+		String dbDriver = props.getProperty("db.driver");
+
+		Connection con = null;
+		Class.forName(dbDriver);
+		con = DriverManager.getConnection(url, user, passwd);
+		return con;
+
 	}
 
-	public List<DBElement> getTable(String table, String col, String[] cond) throws SQLException {
+	protected static Map<String, String> getTable(Connection con, String table) throws SQLException {
+		ArrayList<String> cond = new ArrayList<>();
+		ArrayList<String> col = new ArrayList<>();
+		return DBModel.getTable(con, table, col, cond);
+	}
+
+	protected static Map<String, String> getTable(Connection con, String table, List<String> col, List<String> cond)
+			throws SQLException {
 		String filter = "";
-		Statement st;
+		StringBuilder bld = new StringBuilder();
 		ResultSet rs;
-		String query = "SELECT * FROM" + table + "WHERE 1=1" + filter;
+		HashMap<String, String> ret = new HashMap<>();
 
-		st = this.db.createStatement();
-		rs = st.executeQuery(query);
-		st.close();
-		
-		
-		// countColumns
-		return Collections.emptyList();
+		if (col.size() == cond.size()) {
+			for (int i = 0; i < col.size(); i++) {
+				bld.append(" AND WHERE" + col.get(i) + " = " + cond.get(i));
+			}
+		}
+		filter = bld.toString();
+
+		//TODO Add back filter
+		String query = "SELECT * FROM" + table;
+
+		try (Statement st = con.createStatement()) {
+			rs = st.executeQuery(query);
+		}
+
+		ResultSetMetaData rsmd = rs.getMetaData();
+		int columnCount = rsmd.getColumnCount();
+		while (rs.next()) {
+			for (int i = 1; i <= columnCount; i++) {
+				ret.put(rsmd.getColumnName(i), rs.getString(i));
+			}
+		}
+		return ret;
 	}
 }
---
+++ LF05Web/src/lf05/DBServlet.java
@@ -0,0 +1,62 @@
+package lf05;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+import java.io.*;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Logger lgr = Logger.getLogger(DBServlet.class.getName());
+
+	@Override
+	public void init() throws ServletException {
+		try {
+			FileHandler fh = new FileHandler("Servlet.log");
+			lgr.addHandler(fh);
+		} catch (Exception e) {
+			lgr.log(Level.SEVERE, e.getMessage(), e);
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// stuff
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		String table = request.getParameter("table");
+		Map<String, String> output = new HashMap<>();
+		PrintWriter out = response.getWriter();
+		try {
+			Connection con = DBModel.initDB();
+			out.println("<h1>" + con.toString() + "</h1>");
+			output = DBModel.getTable(con, table);
+		} catch (Exception e) {
+			lgr.log(Level.SEVERE, e.getMessage(), e);
+		}
+
+		out.println("<h1>" + table + "</h1>");
+		output.forEach((k, v) -> out.println("<h1>" + k + ": " + v + "</h1>"));
+
+		/*
+		 * old forwarding testing RequestDispatcher rs =
+		 * request.getRequestDispatcher("welcome.html"); rs.forward(request, response);
+		 */
+	}
+
+	@Override
+	public void destroy() {
+		/*
+		 * end of Servlet life cycle
+		 */
+	}
+}
